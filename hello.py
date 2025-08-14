0f2dff59447029a45408cef40ed8df0a773a70b6
yomerotomero2-pixel <yomerotomero2@gmail.com>
2025-08-14 16:58:14 +0000

    LOWERCASE

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-print("hello, world")
+name = input("what is your name? ")
+print("Hello,",name)
---
+++ indoor.py
@@ -0,0 +1,2 @@
+name = input("what is your name?write it in uppercase ").strip().lower()
+print("Hello", name)
