443c655104345a029a66af54aa3c866fe9893d80
yomerotomero2-pixel <yomerotomero2@gmail.com>
2025-08-29 17:27:17 +0000

    days

---
+++ checkpoint3/life_loop.py
@@ -0,0 +1,18 @@
+def main():
+    day = input("How was your day, was it bad or good? ")
+    option1 = "bad"
+    option2 = "good"
+
+    while True:
+        if day == option1:
+            print("Remember you are a Child of God!:)")
+            break
+        elif day == option2:
+            print("Remember it was thanks to God!:)")
+            break
+    
+# We all have bad days, and when I have a bad days I want to remember that I am a child of God and that I can handle all things with Him, and when I have a good day I want to remember that it was thanks to Him too.
+main()
+
+
+        
