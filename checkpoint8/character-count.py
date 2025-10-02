78905aefe178d77ab4f6390d232ea061a572dd9c
yomerotomero2-pixel <yomerotomero2@gmail.com>
2025-10-02 15:44:06 +0000

    hello

--- checkpoint8/character-count.py
+++ checkpoint8/character-count.py
@@ -3,7 +3,8 @@ def character_counter(message,dictionary,):                   #defining a functi
         dictionary.setdefault(character, 0)                          #with setdefualt if it doesn't exist it will add the value of the key that is the letter of the message to the dictionary
         dictionary[character] += 1                                         # el setdefault es para agregar keys and values that does not exixt in the dictionary
     print(dictionary)
-    print(len(dictionary))
+    #print(len(message))
+    print(f"Total of characters: {sum(dictionary.values())}")                    #this is adding the values of the dictionary 
 
 
 message = input("Enter a message: ")
@@ -11,6 +12,17 @@ dictionary = {}
 character_counter(message,dictionary,)
 
 
+#Alternative 1 to know which character is more repeated
+values_list = list(dictionary.values())             #this is going to create a list with the values of the dictionary(the numbers)
+print(values_list)
+largest_number_index = values_list.index(max(values_list))                     #el index nos da un integer con el elemnto que le preguntemos
+repeated_character = list(dictionary.keys())[largest_number_index]      #we now create a list with the keys(letters) with [] is to look an element
+print(f"The most repeated character is {repeated_character},repeating {dictionary[repeated_character]} times.")
+
+
+#Alternative 2
+largest_number2 = max(dictionary, key=dictionary.get)    #get is going through the dictionary, and the values, el max del value y el key de ese value.
+print(f"The most repeated character is {largest_number2}, repeating {dictionary[largest_number2]} times.")
 
 
 
