495d0b3a2dd682acaaf877bb1edf10d3fe9be170
yomerotomero2-pixel <yomerotomero2@gmail.com>
2025-11-10 19:36:54 +0000

    uni

---
+++ checkpoint12/universities.py
@@ -0,0 +1,5 @@
+university_fair = {
+     "Eastern Arizona College":{"cost": "$1,080 dollars per semester", "location": "Thatcher Arizona"},
+     "Tec de Monterrey":{"cost":"$135,000 per semester","location":"Juarez"},
+     ""
+}
