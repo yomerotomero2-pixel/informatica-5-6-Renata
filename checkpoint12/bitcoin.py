b69627705a193a902e7b17cb9614cf39f53a6c70
yomerotomero2-pixel <yomerotomero2@gmail.com>
2025-11-13 16:39:25 +0000

    bit

--- checkpoint12/bitcoin.py
+++ checkpoint12/bitcoin.py
@@ -5,3 +5,13 @@ API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
 bitcoin = float(input("How many bistcoins do you want to buy? "))
 
 response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+#print(response.json())
+bitcoin_price = float(response.json()["data"]["priceUsd"])
+
+total = round(bitcoin_price * bitcoin, 2)
+print(f"Your total will be: ${total:,.2f}")
+
+
+
+
+
--- checkpoint12/universities.py
+++ checkpoint12/universities.py
@@ -1,13 +1,22 @@
 university_fair = {
-     "UT Paquime":{"cost":1800, "location":16.8 ,"num_majors":4, "official_name": "Universidad Tecnológica de Paquimé"},
-     "Tec de Monterrey":{"cost":135000,"location":305,"num_majors":45, "official_name": "Tecnológico de Monterrey"},
+     "UT PAQUIME":{"cost":1800, "location":16.8 ,"num_majors":4, "official_name": "Universidad Tecnológica de Paquimé"},
+     "TEC DE MONTERREY":{"cost":135000,"location":305,"num_majors":45, "official_name": "Tecnológico de Monterrey"},
      "UACJ":{"cost":3800,"location":35.4,"num_majors":8, "official_name": "Universidad Autónoma de Ciudad Juárez"},
-     "Tec Milenio":{"cost":1200,"location":300,"num_majors":9, "official_name": "Universidad Tecmilenio"},
+     "TEC DE MILENIO":{"cost":1200,"location":300,"num_majors":9, "official_name": "Universidad Tecmilenio"},
      "URN":{"cost":2755,"location":374,"num_majors":3, "official_name": "Universidad Regional del Norte"}
 }
 
 def main():
-     university = input("Please type the name of a university: ")
+     uni = input("Please type the name of a university: ").upper()
      while uni not in university_fair:
-          uni = input("Not found, please try again: ")
+           uni = input("Not found, please try again: ")
+
+import requests
+
+           
+                 
           
+               
+          
+          
+main()
